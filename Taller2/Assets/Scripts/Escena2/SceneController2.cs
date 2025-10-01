992974c9c4cb589a27bdb378b8cbade59d14bae2
sofiaagu <sofia.aguiar@uao.edu.co>
2025-10-01 17:18:23 -0500

    add  lives
    agregue los corazones de la vida

--- Taller2/Assets/Scripts/Escena2/SceneController2.cs
+++ Taller2/Assets/Scripts/Escena2/SceneController2.cs
@@ -1,27 +1,31 @@
-using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
 public class SceneController2 : MonoBehaviour
-{
-    [Header("Prefab de la moneda")]
-    public GameObject coinPrefab;
 
-    [Header("Puntos fijos para las monedas")]
-    public List<GameObject> puntosFijos = new List<GameObject>();
+{
+    public Timer tiempoEscena;
+
+    public TextMeshProUGUI TextCoin1;
+    public TextMeshProUGUI TextCoin2;
+    public TextMeshProUGUI TextCoin3;
+    public TextMeshProUGUI TextTotal;
 
     private void Start()
     {
-        GenerarMonedas();
+       
     }
 
-    private void GenerarMonedas()
+
+    void Update()
     {
-        foreach (GameObject punto in puntosFijos)
+        if (GameManager.Instance != null)
         {
-            if (punto != null && coinPrefab != null)
-            {
-                Instantiate(coinPrefab, punto.transform.position, Quaternion.identity);
-            }
+            TextCoin1.text = GameManager.Instance.ScoreCoin.ToString();
+            TextCoin2.text = GameManager.Instance.ScoreCoin2.ToString();
+            TextCoin3.text = GameManager.Instance.ScoreCoin3.ToString();
+            TextTotal.text = GameManager.Instance.TotalScore.ToString();
         }
     }
 }
